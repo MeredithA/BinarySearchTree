6728805621e37cd5d9aaa94684c37f1bac9a7195
MeredithA <meredith.albrecht00@gmail.com>
2018-03-09 09:25:01 -0600

    Binary tree search.

---
+++ BinaryTree/BinaryTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class BinaryTree
+    {
+        //member variables
+        public Node headNode;
+
+        //constructor
+        private BinaryTree(int data)
+        {
+        }
+
+        //member methods
+        private void Add(int data)
+        {
+            if (headNode == null)
+            {
+                Node nodeToAdd = new Node();
+                headNode = nodeToAdd;
+                return;
+            }
+            Node currentNode = headNode;
+            bool added = false;
+            do
+            {
+                if (data < currentNode.data)
+                {
+                    if (currentNode.leftNode == null)
+                    {
+                        Node newNode = new Node();
+                        currentNode.leftNode = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.leftNode;
+                    }
+                }
+                if (data > currentNode.data)
+                {
+                    if (currentNode.rightNode == null)
+                    {
+                        Node newNode = new Node();
+                        currentNode.rightNode = newNode;
+                        added = true;
+                    }
+                    else
+                    {
+                        currentNode = currentNode.rightNode;
+                    }
+                }
+            } while (!added);
+        }
+
+        public bool Search(int data)
+        {
+            Node currentNode = headNode;
+
+            while (currentNode != null)
+            {
+                if (currentNode.data == data)
+                {
+                    return true;
+                }
+                else if (currentNode.data < data)
+                {
+                    currentNode = currentNode.rightNode;
+                }
+                else if (currentNode.data > data)
+                {
+                    currentNode = currentNode.leftNode;
+                }
+            }
+            return false;
+        }
+
+    }
+}
